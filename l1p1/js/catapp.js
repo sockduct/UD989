8a02826feec88d351502c1ed4ac2a8c95ba52851
James Small <james.r.small@outlook.com>
2017-09-16 16:37:09 -0400

    Update to support list of cats; Bug - nested click handler increments clicks wrong...

--- l1p1/js/catapp.js
+++ l1p1/js/catapp.js
@@ -4,27 +4,41 @@
 
 'use strict';
 
-// How many times has the cat been clicked on?
-var numClicks1 = 0;
-var numClicks2 = 0;
-var cat1Name = 'Zeus';
-var cat2Name = 'Athena';
+// Data
+class cat {
+  constructor(name, picture, clicks) {
+    this.name = name;
+    this.picture = picture;
+    this.clicks = clicks;
+  }
+};
 
-$('#cat_title1').text(cat1Name);
-$('#cat_title2').text(cat2Name);
+var cat1 = new cat('Mercury', 'cat1.jpg', 0);
+var cat2 = new cat('Venus', 'cat2.jpg', 0);
+var cat3 = new cat('Mars', 'cat3.jpg', 0);
+var cat4 = new cat('Jupiter', 'cat4.jpg', 0);
+var cat5 = new cat('Saturn', 'cat5.jpg', 0);
+var cat6 = new cat('Neptune', 'cat6.jpg', 0);
 
-$('#cat_image1').click(function(e) {
-  console.log('You clicked on cat 1!');
-  numClicks1++;
+var cats = [cat1, cat2, cat3, cat4, cat5, cat6];
 
-  // Update click counter
-  $('#catClicks1').text(numClicks1);
-});
 
-$('#cat_image2').click(function(e) {
-  console.log('You clicked on cat 2!');
-  numClicks2++;
+// Program
+for (var i = 0; i < cats.length; i++) {
+  // Current cat
+  var icat = cats[i];
 
-  // Update click counter
-  $('#catClicks2').text(numClicks2);
-});
+  $('#catList').append('<li id="cat' + (i + 1) + '">' + icat.name + '</li>');
+  $('#cat' + (i + 1)).on('click', (function(iSaved, icatSaved) {
+    return function() {
+      console.log('You clicked on cat' + (iSaved + 1) + ' from the list!');
+      $('#catTitle').text(icatSaved.name);
+      $('#catImage').attr('src', 'img/' + icatSaved.picture);
+      $('#catClicks').text(icatSaved.clicks);
+      $('#catImage').on('click', function() {
+        icatSaved.clicks++;
+        $('#catClicks').text(icatSaved.clicks);
+      });
+    };
+  })(i, icat));
+};
